9f10852f098fd96dad0ef5ffcb649a1c41dec26f
deepan <deepanappu1999@gmail.com>
2026-03-14 17:42:51 +0530

    setting up the project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,8 @@
+import { providePrimeNG } from 'primeng/config';
+import Lara from '@primeuix/themes/lara';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -7,6 +10,15 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    providePrimeNG({
+           theme: {
+                preset: Lara,
+                options: {
+                    darkModeSelector: '.my-app-dark', 
+                    cssLayer: false
+                }
+            }
+        })
   ]
 };
