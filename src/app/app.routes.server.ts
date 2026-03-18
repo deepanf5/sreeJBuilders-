e034329a65f154b9fb16a736d08982238b964e28
deepan <deepanappu1999@gmail.com>
2026-03-18 20:11:11 +0530

    fixing the route solution 4

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,7 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'products/edit/:id',
+    path: 'project/:id',
     renderMode: RenderMode.Server,
   },
   {
