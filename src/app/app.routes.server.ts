2d5547b41241ef5338a49f20457cdda95f174a17
deepan <deepanappu1999@gmail.com>
2026-03-18 20:02:50 +0530

    fixing route solution 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,18 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'project/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      // Return an array of parameter objects for each route instance
+      return [
+        { id: '1' },
+        { id: '2' },
+        { id: '3' },
+      ];
+    },
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
