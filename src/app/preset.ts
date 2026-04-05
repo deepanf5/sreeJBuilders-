e066d8dfe31a2eefba7ac5a461667fda5a1a5672
deepan <deepanappu1999@gmail.com>
2026-04-05 11:56:38 +0530

    changing the color code

--- src/app/preset.ts
+++ src/app/preset.ts
@@ -7,18 +7,18 @@ import Aura from '@primeuix/themes/aura';
 const MyGreenPreset = definePreset(Aura, {
     semantic: {
         primary: {
-            50:  '#f6f7f3', // Dew
-            100: '#e8e9e1', // Sage Bone
-            200: '#d1d4c4', // Misty Leaf
-            300: '#a7ae94', // Dusty Moss
-            400: '#8b9376', // Olive Drab
-            500: '#6b705c', // Base Green (Earth)
-            600: '#5a5f4d', // Alpine
-            700: '#4a4e40', // Deep Foliage
-            800: '#3a3d32', // Evergreen
-            900: '#2d362b', // Deep Forest
-            950: '#1a1f18',  // Midnight Jungle
-            1000:'#CD7F32'
+            50:  '#e6e9f3',  // Very Pale Blue
+100: '#c5cee6',  // Soft Sky
+200: '#9eb0d6',  // Light Blue
+300: '#778fc2',  // Powder Blue
+400: '#566fb0',  // Medium Blue
+500: '#3a5399',  // Base Blue
+600: '#2e437b',  // Deep Blue
+700: '#22335c',  // Dark Blue
+800: '#172554',  // Midnight Blue (your main color)
+900: '#0e172f',  // Very Dark Blue
+950: '#070d1a',  // Almost Black Blue
+1000:'#000812'   // Deepest Shade
         },
     }
 });
